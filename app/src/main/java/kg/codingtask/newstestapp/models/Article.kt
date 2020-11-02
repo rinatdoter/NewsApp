e74c321bbe8930643068ec3246be47823a1264ae
rinatdoter <atutorial196@gmail.com>
2020-11-02 18:18:30 +0600

    fixed crash caused by null json value

--- app/src/main/java/kg/codingtask/newstestapp/models/Article.kt
+++ app/src/main/java/kg/codingtask/newstestapp/models/Article.kt
@@ -6,13 +6,13 @@ import java.io.Serializable
 @Entity
 data class Article(
     @PrimaryKey
-    var url: String,
+    var url: String? = "",
     var source: Source,
     var author: String?="",
-    var title: String,
-    var description: String,
-    var urlToImage: String,
-    var publishedAt: String,
+    var title: String? = "",
+    var description: String? = "",
+    var urlToImage: String? = " ",
+    var publishedAt: String? = "",
     var content: String? = "",
     var isBookMarked: Boolean? = false
 ): Serializable
